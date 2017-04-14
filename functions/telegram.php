aaeba96865a70a767f1e4b9c9afb72c9c0cd6fd9
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2017-04-14 18:27:32 +0200

    Create telegram.php

---
+++ functions/telegram.php
@@ -0,0 +1,13 @@
+<?php
+// https://www.domoticz.com/wiki/PHP:_Send_notifications
+function telegram($message)
+{
+	global $telegrambot, $telegramchatid;
+	$url = 'https://api.telegram.org/bot'.$telegrambot.'/sendMessage';
+	$data = array('chat_id' => $telegramchatid, 'text' => $message);
+	$options = array('http' => array('method' => 'POST', 'header' => "Content-Type:application/x-www-form-urlencoded\r\n", 'content' => http_build_query($data)));
+	$context = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	return $result;
+}
+?>
