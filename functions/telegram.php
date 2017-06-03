826ddd9efaf86929794ebacf2af8b4504a9d7480
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2017-06-03 14:00:28 +0200

    Update telegram.php

--- functions/telegram.php
+++ functions/telegram.php
@@ -4,7 +4,7 @@ function telegram($message)
 {
 	global $telegrambot, $telegramchatid;
 	$url = 'https://api.telegram.org/bot'.$telegrambot.'/sendMessage';
-	$data = array('chat_id' => $telegramchatid, 'text' => $message);
+	$data = array('chat_id' => $telegramchatid, 'text' => $message, 'disable_web_page_preview' => 1);
 	$options = array('http' => array('method' => 'POST', 'header' => "Content-Type:application/x-www-form-urlencoded\r\n", 'content' => http_build_query($data)));
 	$context = stream_context_create($options);
 	$result = file_get_contents($url, false, $context);
